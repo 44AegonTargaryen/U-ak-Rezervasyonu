a1f1e670dacd847884b9bd30d5892eeca2e0edac
Mert Okan Yapar <153237750+44AegonTargaryen@users.noreply.github.com>
2024-01-21 16:11:04 +0300

    Add files via upload

---
+++ UcakRezervasyonu-master/UcakRezervasyonu/LokasyonOlustur.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcakRezervasyonu
+{
+    internal class LokasyonOlustur : Lokasyon
+    {
+        public string Ulke { get; set; }
+        public string Sehir { get; set; }
+        public string Havalimani { get; set; }
+
+        public List<string> Lokasyonlar = new List<string>();
+
+        public LokasyonOlustur()
+        {
+            Lokasyonlar.Add("İstanbul / Türkiye, Sabiha Gökçen Havalimanı");
+            Lokasyonlar.Add("İstanbul / Türkiye, Atatürk Havalimanı");
+            Lokasyonlar.Add("Moskova / Rusya, Domodedovo Havalimanı");
+            Lokasyonlar.Add("Tokyo / Japonya, Narita Uluslararası Havalimanı");
+            Lokasyonlar.Add("Muan / Kore, Muan Uluslararası Havalimanı");
+            Lokasyonlar.Add("Sydney / Avustralya, Sydney Kingsford Smith Havalimanı ");
+            Lokasyonlar.Add("Paris / Fransa, Charles de Gaulle Havalimanı");
+        }
+    }
+}
